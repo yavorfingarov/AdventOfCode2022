4504b0bc54381844a8b326a76b8de4fee9638549
Yavor Fingarov <yavor.fingarov@outlook.com>
2022-12-03 11:50:55 +0200

    Added Day03

---
+++ AdventOfCode2022.Tests/Day03Tests.cs
@@ -0,0 +1,31 @@
+﻿namespace AdventOfCode2022.Tests
+{
+    public class Day03Tests : DayTests
+    {
+        public override List<TestCase> TestCasesPart1 => new()
+        {
+            new(157, new[]
+            {
+                "vJrwpWtwJgWrhcsFMMfFFhFp",
+                "jqHRNqRjqzjGDLGLrsFMfFZSrLrFZsSL",
+                "PmmdzqPrVvPwwTWBwg",
+                "wMqvLMZHhHMvwLHjbvcjnnSBnvTQFn",
+                "ttgJtRGJQctTZtZT",
+                "CrZsJsPPZsGzwwsLwLmpwMDw"
+            })
+        };
+
+        public override List<TestCase> TestCasesPart2 => new()
+        {
+            new(70, new[]
+            {
+                "vJrwpWtwJgWrhcsFMMfFFhFp",
+                "jqHRNqRjqzjGDLGLrsFMfFZSrLrFZsSL",
+                "PmmdzqPrVvPwwTWBwg",
+                "wMqvLMZHhHMvwLHjbvcjnnSBnvTQFn",
+                "ttgJtRGJQctTZtZT",
+                "CrZsJsPPZsGzwwsLwLmpwMDw"
+            })
+        };
+    }
+}
---
+++ AdventOfCode2022/Day03/Day03.cs
@@ -0,0 +1,39 @@
+﻿namespace AdventOfCode2022.Day03
+{
+    public class Day03 : IDay
+    {
+        public object Part1(string input)
+        {
+            return input.GetLines()
+                .Select(l =>
+                {
+                    var chunks = l.Chunk(l.Length / 2);
+
+                    return chunks.First().Intersect(chunks.Last());
+                })
+                .Select(d => d.Sum(c => c.ToPriority()))
+                .Sum();
+        }
+
+        public object Part2(string input)
+        {
+            return input.GetLines()
+                .Chunk(3)
+                .Select(r => r[0].Intersect(r[1]).Intersect(r[2]))
+                .Sum(c => c.Single().ToPriority());
+        }
+    }
+
+    public static class ExtensionMethods
+    {
+        public static int ToPriority(this char c)
+        {
+            if (char.IsLower(c))
+            {
+                return c - 96;
+            }
+
+            return c - 38;
+        }
+    }
+}
