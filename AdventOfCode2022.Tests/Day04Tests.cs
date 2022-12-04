fd0072d6cfb41f40c63906cf921767a4234a4045
Yavor Fingarov <yavor.fingarov@outlook.com>
2022-12-04 10:15:59 +0200

    Added Day04

---
+++ AdventOfCode2022.Tests/Day04Tests.cs
@@ -0,0 +1,31 @@
+﻿namespace AdventOfCode2022.Tests
+{
+    public class Day04Tests : DayTests
+    {
+        public override List<TestCase> TestCasesPart1 => new()
+        {
+            new(2, new[]
+            {
+                "2-4,6-8",
+                "2-3,4-5",
+                "5-7,7-9",
+                "2-8,3-7",
+                "6-6,4-6",
+                "2-6,4-8"
+            })
+        };
+
+        public override List<TestCase> TestCasesPart2 => new()
+        {
+            new(4, new[]
+            {
+                "2-4,6-8",
+                "2-3,4-5",
+                "5-7,7-9",
+                "2-8,3-7",
+                "6-6,4-6",
+                "2-6,4-8"
+            })
+        };
+    }
+}
---
+++ AdventOfCode2022/Day04/Day04.cs
@@ -0,0 +1,37 @@
+﻿namespace AdventOfCode2022.Day04
+{
+    public class Day04 : IDay
+    {
+        public object Part1(string input)
+        {
+            return input.GetLines()
+                .Select(GetPairs)
+                .Count(p => (p.Elf1.From >= p.Elf2.From && p.Elf1.To <= p.Elf2.To) ||
+                    (p.Elf2.From >= p.Elf1.From && p.Elf2.To <= p.Elf1.To));
+        }
+
+        public object Part2(string input)
+        {
+            return input.GetLines()
+                .Select(GetPairs)
+                .Count(p => (p.Elf1.From >= p.Elf2.From && p.Elf1.From <= p.Elf2.To) ||
+                    (p.Elf1.To <= p.Elf2.To && p.Elf1.To >= p.Elf2.From) ||
+                    (p.Elf2.From >= p.Elf1.From && p.Elf2.From <= p.Elf1.To) ||
+                    (p.Elf2.To <= p.Elf1.To && p.Elf2.To >= p.Elf1.From));
+        }
+
+        private static ((int From, int To) Elf1, (int From, int To) Elf2) GetPairs(string line)
+        {
+            var pairs = line.Split(',')
+                .Select(p =>
+                {
+                    var numbers = p.Split('-');
+
+                    return (From: int.Parse(numbers[0]), To: int.Parse(numbers[1]));
+                })
+                .ToArray();
+
+            return (Elf1: pairs[0], Elf2: pairs[1]);
+        }
+    }
+}
