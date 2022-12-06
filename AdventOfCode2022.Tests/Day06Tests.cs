8d02b6ab75a627711ac12ead29afcba7ca93a088
Yavor Fingarov <yavor.fingarov@outlook.com>
2022-12-06 09:55:59 +0200

    Added Day06

---
+++ AdventOfCode2022.Tests/Day06Tests.cs
@@ -0,0 +1,23 @@
+﻿namespace AdventOfCode2022.Tests
+{
+    public class Day06Tests : DayTests
+    {
+        public override List<TestCase> TestCasesPart1 => new()
+        {
+            new(7, new[] { "mjqjpqmgbljsphdztnvjfqwrcgsmlb" }),
+            new(5, new[] { "bvwbjplbgvbhsrlpgdmjqwftvncz" }),
+            new(6, new[] { "nppdvjthqldpwncqszvftbrmjlhg" }),
+            new(10, new[] { "nznrnfrfntjfmvfwmzdfjlvtqnbhcprsg" }),
+            new(11, new[] { "zcfzfwzzqfrljwzlrfnpqdbhtmscgvjw" }),
+        };
+
+        public override List<TestCase> TestCasesPart2 => new()
+        {
+            new(19, new[] { "mjqjpqmgbljsphdztnvjfqwrcgsmlb" }),
+            new(23, new[] { "bvwbjplbgvbhsrlpgdmjqwftvncz" }),
+            new(23, new[] { "nppdvjthqldpwncqszvftbrmjlhg" }),
+            new(29, new[] { "nznrnfrfntjfmvfwmzdfjlvtqnbhcprsg" }),
+            new(26, new[] { "zcfzfwzzqfrljwzlrfnpqdbhtmscgvjw" }),
+        };
+    }
+}
---
+++ AdventOfCode2022/Day06/Day06.cs
@@ -0,0 +1,33 @@
+﻿namespace AdventOfCode2022.Day06
+{
+    public class Day06 : IDay
+    {
+        public object Part1(string input)
+        {
+            for (var i = 3; i < input.Length; i++)
+            {
+                var hashSet = new HashSet<char>(input.Substring(i - 3, 4));
+                if (hashSet.Count == 4)
+                {
+                    return i + 1;
+                }
+            }
+
+            throw new InvalidOperationException();
+        }
+
+        public object Part2(string input)
+        {
+            for (var i = 13; i < input.Length; i++)
+            {
+                var hashSet = new HashSet<char>(input.Substring(i - 13, 14));
+                if (hashSet.Count == 14)
+                {
+                    return i + 1;
+                }
+            }
+
+            throw new InvalidOperationException();
+        }
+    }
+}
