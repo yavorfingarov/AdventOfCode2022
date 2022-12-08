f4281434f8184da07ffc9df22d17444b027845e7
Yavor Fingarov <yavor.fingarov@outlook.com>
2022-12-08 08:53:43 +0200

    Added Day08

---
+++ AdventOfCode2022.Tests/Day08Tests.cs
@@ -0,0 +1,29 @@
+﻿namespace AdventOfCode2022.Tests
+{
+    public class Day08Tests : DayTests
+    {
+        public override List<TestCase> TestCasesPart1 => new()
+        {
+            new(21, new[]
+            {
+                "30373",
+                "25512",
+                "65332",
+                "33549",
+                "35390"
+            })
+        };
+
+        public override List<TestCase> TestCasesPart2 => new()
+        {
+            new(8, new[]
+            {
+                "30373",
+                "25512",
+                "65332",
+                "33549",
+                "35390"
+            })
+        };
+    }
+}
---
+++ AdventOfCode2022/Day08/Day08.cs
@@ -0,0 +1,159 @@
+﻿namespace AdventOfCode2022.Day08
+{
+    public class Day08 : IDay
+    {
+        public object Part1(string input)
+        {
+            var grid = input.GetLines()
+                .Select(l => l.Select(c => c - 48).ToArray())
+                .ToArray();
+            var visibleCount = 0;
+            for (var y = 0; y < grid.Length; y++)
+            {
+                for (var x = 0; x < grid[y].Length; x++)
+                {
+                    if (IsVisibleFromNorth(x, y, grid) ||
+                        IsVisibleFromSouth(x, y, grid) ||
+                        IsVisibleFromWest(x, y, grid) ||
+                        IsVisibleFromEast(x, y, grid))
+                    {
+                        visibleCount++;
+                    }
+                }
+            }
+
+            return visibleCount;
+        }
+
+        public object Part2(string input)
+        {
+            var grid = input.GetLines()
+                .Select(l => l.Select(c => c - 48).ToArray())
+                .ToArray();
+            var scenicScores = new List<int>();
+            for (var y = 0; y < grid.Length; y++)
+            {
+                for (var x = 0; x < grid[y].Length; x++)
+                {
+                    var scenicScore = GetVisibleFromNorthCount(x, y, grid) * GetVisibleFromSouthCount(x, y, grid) *
+                        GetVisibleFromWestCount(x, y, grid) * GetVisibleFromEastCount(x, y, grid);
+                    scenicScores.Add(scenicScore);
+                }
+            }
+
+            return scenicScores.Max();
+        }
+
+        private static bool IsVisibleFromNorth(int x, int y, int[][] grid)
+        {
+            for (var currentY = y - 1; currentY >= 0; currentY--)
+            {
+                if (grid[y][x] <= grid[currentY][x])
+                {
+                    return false;
+                }
+            }
+
+            return true;
+        }
+
+        private static bool IsVisibleFromSouth(int x, int y, int[][] grid)
+        {
+            for (var currentY = y + 1; currentY < grid.Length; currentY++)
+            {
+                if (grid[y][x] <= grid[currentY][x])
+                {
+                    return false;
+                }
+            }
+
+            return true;
+        }
+
+        private static bool IsVisibleFromWest(int x, int y, int[][] grid)
+        {
+            for (var currentX = x - 1; currentX >= 0; currentX--)
+            {
+                if (grid[y][x] <= grid[y][currentX])
+                {
+                    return false;
+                }
+            }
+
+            return true;
+        }
+
+        private static bool IsVisibleFromEast(int x, int y, int[][] grid)
+        {
+            for (var currentX = x + 1; currentX < grid[y].Length; currentX++)
+            {
+                if (grid[y][x] <= grid[y][currentX])
+                {
+                    return false;
+                }
+            }
+
+            return true;
+        }
+
+        private static int GetVisibleFromNorthCount(int x, int y, int[][] grid)
+        {
+            var visibleCount = 0;
+            for (var currentY = y - 1; currentY >= 0; currentY--)
+            {
+                visibleCount++;
+                if (grid[y][x] <= grid[currentY][x])
+                {
+                    break;
+                }
+            }
+
+            return visibleCount;
+        }
+
+        private static int GetVisibleFromSouthCount(int x, int y, int[][] grid)
+        {
+            var visibleCount = 0;
+            for (var currentY = y + 1; currentY < grid.Length; currentY++)
+            {
+                visibleCount++;
+                if (grid[y][x] <= grid[currentY][x])
+                {
+                    break;
+                }
+            }
+
+            return visibleCount;
+        }
+
+        private static int GetVisibleFromWestCount(int x, int y, int[][] grid)
+        {
+            var visibleCount = 0;
+            for (var currentX = x - 1; currentX >= 0; currentX--)
+            {
+                visibleCount++;
+                if (grid[y][x] <= grid[y][currentX])
+                {
+                    break;
+                }
+            }
+
+            return visibleCount;
+        }
+
+        private static int GetVisibleFromEastCount(int x, int y, int[][] grid)
+        {
+            var visibleCount = 0;
+            for (var currentX = x + 1; currentX < grid[y].Length; currentX++)
+            {
+                visibleCount++;
+                if (grid[y][x] <= grid[y][currentX])
+                {
+                    break;
+                }
+            }
+
+            return visibleCount;
+        }
+    }
+}
